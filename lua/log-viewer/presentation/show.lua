9924a838a65ae1b8ae51e80940f066913775aec8
eetann <hideharu092@gmail.com>
2025-01-27 19:02:16 +0900

    feat: ファイル更新時に自動読み込みとスクロールをパース後でも実装

--- lua/log-viewer/presentation/show.lua
+++ lua/log-viewer/presentation/show.lua
@@ -1,12 +1,10 @@
-local ParseFile = require("log-viewer.usecase.parse_file")
 local View = require("log-viewer.presentation.view")
 local M = {}
 
 function M.show()
-  local file_path = vim.lsp.get_log_path()
-  local content = ParseFile:new():execute(file_path)
   local view = View:new()
-  view:set_content(content)
+  view:set_content()
+  view:set_scroll()
 end
 
 return M
--- lua/log-viewer/presentation/view.lua
+++ lua/log-viewer/presentation/view.lua
@@ -1,10 +1,16 @@
+local ParseFile = require("log-viewer.usecase.parse_file")
+
+local file_path = vim.lsp.get_log_path()
+local is_last_line = fales
+
 ---@class log-viewer.View
 ---@field buf integer
 ---@field win integer
+---@field is_last_line boolean
 View = {}
 View.__index = View
 
-function View.new()
+function View:new()
   local buf = vim.api.nvim_create_buf(false, true)
   if not buf then
     error("Failed to create buffer")
@@ -17,13 +23,46 @@ function View.new()
   vim.cmd("setfiletype nvim-lsp-log")
   vim.keymap.set("n", "q", ("<CMD>bdelete %d<CR>"):format(buf))
 
-  return setmetatable({ buf = buf, win = win }, View)
+  return setmetatable({ buf = buf, win = win, is_last_line = false }, View)
 end
 
----@param content string
-function View:set_content(content)
+function View:set_content()
+  vim.api.nvim_set_option_value("modifiable", true, { buf = self.buf })
+  local content = ParseFile:new():execute(file_path)
   vim.api.nvim_buf_set_lines(self.buf, 0, -1, false, vim.fn.split(content, "\n"))
+  -- TODO: ここで保存に関する設定をしないとだめっぽい
   vim.api.nvim_set_option_value("modifiable", false, { buf = self.buf })
+end
+
+function View:set_scroll()
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    buffer = self.buf,
+    callback = function()
+      vim.print(self.buf)
+      local line_count = vim.api.nvim_buf_line_count(self.buf)
+      self.is_last_line = vim.api.nvim_win_get_cursor(self.win)[1] == line_count
+    end,
+  })
+
+  local watch = vim.uv.new_fs_event()
+  if not watch then
+    return
+  end
+  local event_callback = function(err)
+    vim.schedule(function()
+      if err then
+        vim.print(err)
+      else
+        self:set_content()
+        if self.is_last_line then
+          vim.api.nvim_win_call(self.win, function()
+            vim.cmd("normal! G")
+          end)
+        end
+      end
+    end)
+  end
+  vim.uv.fs_event_start(watch, file_path, {}, event_callback)
 end
 
 return View
