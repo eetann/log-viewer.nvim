f43c2d13e0b59a2786c5932650b9160a958f14b3
eetann <hideharu092@gmail.com>
2025-02-10 19:01:17 +0900

    build: ドキュメントの先頭の区切りを自動削除
    mini.nvim/scripts/minidoc.lua に書いてあった

--- scripts/generate_doc.lua
+++ scripts/generate_doc.lua
@@ -1,4 +1,13 @@
 local MiniDoc = require("mini.doc")
+local hooks = vim.deepcopy(MiniDoc.default_hooks)
+
+hooks.write_pre = function(lines)
+  -- Remove first two lines with `======` and `------` delimiters to comply
+  -- with `:h local-additions` template
+  table.remove(lines, 1)
+  table.remove(lines, 1)
+  return lines
+end
 MiniDoc.generate({
   "lua/lsp-dev/init.lua",
   "lua/lsp-dev/presentation/show_log/show_log.lua",
@@ -7,4 +16,4 @@ MiniDoc.generate({
   "lua/lsp-dev/presentation/show_capabilities.lua",
   "lua/lsp-dev/presentation/command.lua",
   "lua/lsp-dev/presentation/api.lua",
-}, "doc/lsp-dev.txt", {})
+}, "doc/lsp-dev.txt", { hooks = hooks })
