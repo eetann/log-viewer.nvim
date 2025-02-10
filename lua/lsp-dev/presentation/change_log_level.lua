5d068aacbdd1c2277fedb3b3bd27ee41c64d24fd
eetann <hideharu092@gmail.com>
2025-02-10 17:24:02 +0900

    feat: 現在のログレベルも分かるようにした

--- lua/lsp-dev/presentation/change_log_level.lua
+++ lua/lsp-dev/presentation/change_log_level.lua
@@ -2,6 +2,7 @@
 ---@toc_entry Change log level
 ---@text
 --- You can quickly switch between LSP logging level with a popup.
+--- The popup also displayes the current level.
 
 local M = {}
 
@@ -14,52 +15,53 @@ local function set_log_level(level)
   vim.lsp.set_log_level(level)
 end
 
-local lines = {
-  Menu.item("(T) TRACE", { key = "t", level = "TRACE" }),
-  Menu.item("(D) DEBUG", { key = "d", level = "DEBUG" }),
-  Menu.item("(I) INFO", { key = "i", level = "INFO" }),
-  Menu.item("(W) WARN", { key = "w", level = "WARN" }),
-  Menu.item("(E) ERROR", { key = "e", level = "ERROR" }),
-  Menu.item("(O) OFF", { key = "o", level = "OFF" }),
-}
-
-local menu = Menu({
-  position = "50%",
-  size = {
-    width = 22,
-  },
-  border = {
-    style = "single",
-    padding = { 1, 2 },
-    text = {
-      top = "[Choose log level]",
-      top_align = "center",
-    },
-  },
-  win_options = {
-    winhighlight = "Normal:Normal,FloatBorder:Normal",
-  },
-}, {
-  lines = lines,
-  keymap = {
-    focus_next = { "j", "<Down>", "<Tab>" },
-    focus_prev = { "k", "<Up>", "<S-Tab>" },
-    close = { "<Esc>", "<C-c>", "q" },
-    submit = { "<CR>", "<Space>" },
-  },
-  on_submit = function(item)
-    set_log_level(item.level)
-  end,
-})
-
-for _, line in ipairs(lines) do
-  menu:map("n", line.key, function()
-    menu:unmount()
-    set_log_level(line.level)
-  end)
-end
-
 function M.change_log_level()
+  local lines = {
+    Menu.separator("Now: " .. vim.lsp.log.levels[vim.lsp.log.get_level()]),
+    Menu.item("[T] TRACE", { key = "t", level = "TRACE" }),
+    Menu.item("[D] DEBUG", { key = "d", level = "DEBUG" }),
+    Menu.item("[I] INFO", { key = "i", level = "INFO" }),
+    Menu.item("[W] WARN (Neovim default)", { key = "w", level = "WARN" }),
+    Menu.item("[E] ERROR", { key = "e", level = "ERROR" }),
+    Menu.item("[O] OFF", { key = "o", level = "OFF" }),
+  }
+
+  local menu = Menu({
+    position = "50%",
+    size = {
+      width = 25,
+    },
+    border = {
+      style = "single",
+      padding = { 1, 2 },
+      text = {
+        top = "[Choose log level]",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:Normal",
+    },
+  }, {
+    lines = lines,
+    keymap = {
+      focus_next = { "j", "<Down>", "<Tab>" },
+      focus_prev = { "k", "<Up>", "<S-Tab>" },
+      close = { "<Esc>", "<C-c>", "q" },
+      submit = { "<CR>", "<Space>" },
+    },
+    on_submit = function(item)
+      set_log_level(item.level)
+    end,
+  })
+
+  for _, line in ipairs(lines) do
+    menu:map("n", line.key, function()
+      menu:unmount()
+      set_log_level(line.level)
+    end)
+  end
+
   menu:mount()
 end
 
